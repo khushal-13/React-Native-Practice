d62ce91249a517c5263251c03cef1a5b5f8e4ed7
Khushal Jogi <khushaljogi7@gmail.com>
2025-08-19 21:45:18 +0530

    Practing

---
+++ habit-tracker/app/index.js
@@ -0,0 +1,13 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+
+const index = () => {
+  return (
+    <View>
+      <Text>indexxxx</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
